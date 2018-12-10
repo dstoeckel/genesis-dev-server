ccba43a5cfccfc0da0b4b219161f2d7ef7b36599
Moritz Onken <moritz.onken@merckgroup.com>
2018-12-10 17:11:32 +0100

    linted

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const crypto = require('crypto'),
-  cookieParser = require('cookie-parser'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  path = require('path'),
-  url = require('url'),
-  fs = require('fs'),
-  assert = require('assert'),
-  utils = require('./utils'),
-  Lambda = require('./lib/lambda'),
-  WebpackDevServer = require('webpack-dev-server');
+const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const assert = require('assert');
+const WebpackDevServer = require('webpack-dev-server');
+
+const utils = require('./utils');
+const Lambda = require('./lib/lambda');
 
 module.exports = (args, config) => {
-
   const app = express();
 
   app.use(cookieParser());
@@ -23,8 +23,8 @@ module.exports = (args, config) => {
   });
 
   app.all('/api/*', (req, res) => {
-    let parsedUrl = url.parse(req.url, true);
-    let event = {
+    const parsedUrl = url.parse(req.url, true);
+    const event = {
       resource: '/api/{proxy+}',
       path: req.path,
       httpMethod: req.method,
@@ -47,36 +47,68 @@ module.exports = (args, config) => {
       body: req.body instanceof Buffer ? req.body.toString() : null,
       isBase64Encoded: false,
     };
-    let task = path.resolve(args.lambdaBaseDirectory, 'main');
-    let lambda = config.lambda['main'];
+    const task = path.resolve(args.lambdaBaseDirectory, 'main');
+    const lambda = config.lambda.main;
     lambda.path = task;
-    lambda.invoke(event).then(out => {
-      assert.equal(typeof out.statusCode, 'number', 'statusCode must be a number.');
-      res.status(out.statusCode);
-      // TODO: isBase64Encoded
-      // TODO: headers
-      res.send(out.body);
-    }).catch(err => {
-      console.error(err.toString());
-      res.status(502);
-      res.json({ message: 'Internal server error' });
-    });
+    lambda
+      .invoke(event)
+      .then((out) => {
+        assert.equal(typeof out.statusCode, 'number', 'statusCode must be a number.');
+        res.status(out.statusCode);
+        // TODO: isBase64Encoded
+        // TODO: headers
+        res.send(out.body);
+      })
+      .catch((err) => {
+        console.error(err.toString());
+        res.status(502);
+        res.json({ message: 'Internal server error' });
+      });
   });
 
   app.post('/oauth2/user', (req, res) => {
-    res.json({ "at_hash": "U1zvHrfQFBOeBiHapVF23g", "sub": "824ebb6f-dd89-4062-9156-8743043733fd", "cognito:groups": ["us-east-2_ytLIDC5V6_Merck"], "email_verified": false, "iss": "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_ytLIDC5V6", "cognito:username": "Merck_M252249", "preferred_username": "M252249@eu.merckgroup.com", "given_name": "Moritz", "aud": "6ofje021s1673b12mtid3qsubo", "identities": [{ "userId": "M252249", "providerName": "Merck", "providerType": "SAML", "issuer": "https://sts.windows.net/db76fb59-a377-4120-bc54-59dead7d39c9/", "primary": "true", "dateCreated": "1506095170719" }], "token_use": "id", "auth_time": 1527689699, "exp": 1527693299, "iat": 1527689699, "family_name": "Onken", "email": "moritz.onken@merckgroup.com" });
+    res.json({
+      at_hash: 'U1zvHrfQFBOeBiHapVF23g',
+      sub: '824ebb6f-dd89-4062-9156-8743043733fd',
+      'cognito:groups': ['us-east-2_ytLIDC5V6_Merck'],
+      email_verified: false,
+      iss: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_ytLIDC5V6',
+      'cognito:username': 'Merck_M252249',
+      preferred_username: 'M252249@eu.merckgroup.com',
+      given_name: 'Moritz',
+      aud: '6ofje021s1673b12mtid3qsubo',
+      identities: [
+        {
+          userId: 'M252249',
+          providerName: 'Merck',
+          providerType: 'SAML',
+          issuer: 'https://sts.windows.net/db76fb59-a377-4120-bc54-59dead7d39c9/',
+          primary: 'true',
+          dateCreated: '1506095170719',
+        },
+      ],
+      token_use: 'id',
+      auth_time: 1527689699,
+      exp: 1527693299,
+      iat: 1527689699,
+      family_name: 'Onken',
+      email: 'moritz.onken@merckgroup.com',
+    });
   });
 
   app.post('/oauth2/refresh', (req, res) => {
-    let token = crypto.randomBytes(16).toString('hex');
+    const token = crypto.randomBytes(16).toString('hex');
     res.cookie('xsrf-token', token, { expires: new Date(Date.now() + 3600000) });
   });
 
   if (fs.existsSync(args.webpackConfig)) {
-    const webpack = require('webpack'),
-      config = require(path.resolve(args.webpackConfig)),
-      compiler = webpack(config),
-      server = new WebpackDevServer(compiler, config.devServer);
+    const webpack = require('webpack');
+
+    const config = require(path.resolve(args.webpackConfig));
+
+    const compiler = webpack(config);
+
+    const server = new WebpackDevServer(compiler, config.devServer);
     app.use(server.app);
   } else {
     app.get('/', (req, res) => {
@@ -84,8 +116,8 @@ module.exports = (args, config) => {
       <h1>Genesis Dev Server</h1>
       <p>Open the browser's developer tools to access the API Gateway or enable the webpack dev server (<code>--webpack</code>).</p>
     `);
-    })
+    });
   }
 
   return app;
-}
+};
